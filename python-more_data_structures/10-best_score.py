34723d58fe4520474ed784fe5c83454c4438a714
semed0055 <13166@holbertonstudents.com>
2026-03-22 16:05:30 +0000

    Task 10: Find the key with the biggest integer value

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    # İlk elementi başlanğıc kimi götürürük
+    best_key = list(a_dictionary.keys())[0]
+    
+    for key, value in a_dictionary.items():
+        if value > a_dictionary[best_key]:
+            best_key = key
+            
+    return best_key
