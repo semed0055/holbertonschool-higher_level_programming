9e138546d86be1031bd9dc5c3144d23284ecdff0
semed0055 <13166@holbertonstudents.com>
2026-03-22 15:58:54 +0000

    Task 7: Update dictionary with key/value pair

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    # Əgər açar varsa dəyəri dəyişir, yoxdursa yeni açar/dəyər əlavə edir
+    a_dictionary[key] = value
+    return a_dictionary
