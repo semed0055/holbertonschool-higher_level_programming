2564d7e3174405a51629cb7c03da095631c6eaa6
semed0055 <13166@holbertonstudents.com>
2026-04-10 17:54:21 +0000

    Fix all remaining test cases for Task 7

---
+++ python-inharitance/7-base_geometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module defining the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    A class representing base geometry.
+    """
+
+    def area(self):
+        """
+        Public instance method that raises an Exception.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates value as a positive integer.
+
+        Args:
+            name (str): The name of the value (always a string).
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: if value is not an integer.
+            ValueError: if value is <= 0.
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
