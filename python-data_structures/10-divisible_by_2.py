a1697bb6b9fc1ba42c6284d7d1a09ebd038ed704
semed0055 <13166@holbertonstudents.com>
2026-04-25 12:47:35 +0000

    Fix task 10: divisible_by_2 with PEP8 compliance

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list is None:
-        return None
-    
-    result = []
-    for num in my_list:
-        if num % 2 == 0:
-            result.append(True)
+    """Finds all multiples of 2 in a list."""
+    new_list = []
+    for i in my_list:
+        if i % 2 == 0:
+            new_list.append(True)
         else:
-            result.append(False)
-    return result
+            new_list.append(False)
+    return new_list
