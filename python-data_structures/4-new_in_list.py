53f671e09a9d6d3b1edf173fdcb0e90cece38d44
semed0055 <13166@holbertonstudents.com>
2026-04-25 12:43:25 +0000

    Fix task 4: new_in_list with PEP8 compliance

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    # Siyahının kopyasını yaradırıq
+    """Replaces an element in a list without modifying the original."""
     copy_list = my_list[:]
-    
-    # İndeksi yoxlayırıq
     if idx < 0 or idx >= len(my_list):
         return copy_list
-    
-    # Dəyişikliyi yalnız kopya üzərində edirik
     copy_list[idx] = element
     return copy_list
