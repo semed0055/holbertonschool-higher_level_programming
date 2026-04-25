77be988321871b1d096ed55e8aaddcc9576e83a0
semed0055 <13166@holbertonstudents.com>
2026-04-25 12:40:56 +0000

    Fix task 10: best_score with PEP8 compliance

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    """Returns a key with the biggest integer value."""
     if not a_dictionary:
         return None
 
-    # İlk elementi başlanğıc kimi götürürük
-    best_key = list(a_dictionary.keys())[0]
-    
-    for key, value in a_dictionary.items():
-        if value > a_dictionary[best_key]:
-            best_key = key
-            
-    return best_key
+    max_key = list(a_dictionary.keys())[0]
+    for key in a_dictionary:
+        if a_dictionary[key] > a_dictionary[max_key]:
+            max_key = key
+    return max_key
