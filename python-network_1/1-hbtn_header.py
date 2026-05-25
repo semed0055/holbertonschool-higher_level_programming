4e31cef21d93a942022a11f7edf1be623dc5c5c3
semed0055 <his_actual_email@gmail.com>
2026-05-25 19:39:44 +0000

    Fix 1-hbtn_header.py: add __main__ guard and fix docstring

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Sends a request to a URL and displays the X-Request-Id header value"""
+"""Script that takes a URL, sends a request, and displays the X-Request-Id
+header value from the response"""
 import urllib.request
 import sys
 
-url = sys.argv[1]
-req = urllib.request.Request(url, headers={'cfclearance': 'true'})
-with urllib.request.urlopen(req) as response:
-    print(response.headers.get('X-Request-Id'))
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url, headers={'cfclearance': 'true'})
+    with urllib.request.urlopen(req) as response:
+        print(response.headers.get('X-Request-Id'))
